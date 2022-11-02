617419f89e487821bdb6c5f4433be94bfda1c2e2
Zachary Koepf <koepfzm@gmail.com>
2022-11-02 15:38:47 -0400

    adding transfer model

---
+++ capstone/TenmoClient/Models/SendTransfer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenmoClient.Models
+{
+    // model for creating a send money transfer
+    public class SendTransfer
+    {
+        public decimal StartingBalance { get; set; }
+        public decimal TransferAmount { get; set; }
+
+        public bool TransferStatus { get; set; } = true;
+
+        public string ToUsername { get; set; }
+        public string FromUsername { get; set; }
+        
+        public int TransferType { get; set; }
+        public SendTransfer ()
+        {
+
+        }
+
+    }
+}
